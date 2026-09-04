72213351f5ca7d835654c7cabb67915348031cb3
Vyacheslav <slavik.kurganov.26@mail.ru>
2026-09-04 14:36:57 +0300

    Удалил bootstrap и файлы для теста работы webpack

--- src/js/testModule/test.js
+++
@@ -1,7 +0,0 @@
-import './testStyle.css'
-export const createDiv = () => {
-    const div = document.createElement('div');
-    div.classList.add('test-div');
-    div.textContent = 'Что-то написал'
-    document.body.append(div);
-}
